b4be0f1819c3545a6b06d46b4e14425e143f99f9
Gabriel Vinicius Rebouças da Silva <202506150241@alunos.estacio.br>
2025-09-05 17:41:15 +0000

    Refatorar o código do jogo Batalha Naval para usar constantes na definição do tabuleiro e melhorar a legibilidade. Adicionar posicionamento de navios diagonais e verificar limites adequadamente.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,11 +1,14 @@
-
 #include <stdio.h>
 
+// Tabuleiro 
+
+#define LINHAS 10
+#define COLUNAS 10
 
 int main (){
 
     // Tabuleiro
-    int tabuleiro[10][10];
+    int tabuleiro[LINHAS][COLUNAS];
     int agua = 0;
 
     // Navios
@@ -21,11 +24,20 @@ int main (){
     int navio2_Linha_Inicial = 5;
     int navio2_Coluna_Inicial = 5;
 
+
+    // Navio 3 (Diagonal 1)
+    int navio3_Linha_Inicial = 7;
+    int navio3_Coluna_Inicial = 7;
+
+    // Navio 4 (Diagonal 2)
+
+    int navio4_Linha_Inicial = 7;
+    int navio4_Coluna_Inicial = 2;
     
     // Tamanho do Tabuleiro.
-    for (int i = 0; i < 10; i ++){
+    for (int i = 0; i < LINHAS; i ++){
 
-        for(int j = 0; j < 10; j++){
+        for(int j = 0; j < COLUNAS; j++){
             tabuleiro[i][j] = agua;
 
         }
@@ -34,7 +46,7 @@ int main (){
     // Posicionamento do Navio 1. (Horizontal)
 
     // Verifica se o navio não sai dos limites do Tabuleiro.
-    if(navio1_Coluna_Inicial + 3 > 10){
+    if(navio1_Coluna_Inicial + 3 > COLUNAS){
         printf("Erro: o navio sai dos limites do Tabuleiro! \n");
         return 1;
 
@@ -49,13 +61,13 @@ int main (){
     // Posicionamento do Navio 2. (Vertical)
 
     //Verifica se o navio não sai dos limites do Tabuleiro.
-    if(navio2_Linha_Inicial + 3 > 10){
+    if(navio2_Linha_Inicial + navio > LINHAS){
         printf("Erro: o navio sai dos limites do Tabuleiro! \n");
         return 1;
     }
 
     //Verifica se alguma posição está ocupada.
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < navio; i++){
 
         if(tabuleiro[navio2_Linha_Inicial + i][navio2_Coluna_Inicial] == navio){
             printf("Erro: Um navio já está ocupando esta posição. \n");
@@ -65,19 +77,70 @@ int main (){
 
     // Se as verificações não mostrou erros, o navio será posicionado
 
-    for(int i = 0; i < 3; i ++){
+    for(int i = 0; i < navio; i ++){
 
         // Posiciona o navio na vertical a partir da linha e coluna inicial.
         tabuleiro[navio2_Linha_Inicial + i][navio2_Coluna_Inicial] = navio;
     }
 
+
+    //Posicionamento Navio 3. (Diagonal 1)
+
+    //Verifica se o navio não sai dos limites do Tabuleiro.
+    if(navio3_Linha_Inicial + navio > LINHAS && navio3_Coluna_Inicial + navio > COLUNAS){
+        printf("Erro: o navio sai dos limites do Tabuleiro! \n");
+        return 1;
+    }
+
+    //Verifica se alguma posição está ocupada.
+    for(int i = 0; i < navio; i++){
+
+        if(tabuleiro[navio3_Linha_Inicial + i][navio3_Coluna_Inicial + i] == navio){
+            printf("Erro: Um navio já está ocupando esta posição. \n");
+            return 1;
+        }
+    }
+
+    // Se as verificações não mostrou erros, o navio será posicionado
+
+    for(int i = 0; i < navio; i ++){
+
+        // Posiciona o navio na vertical a partir da linha e coluna inicial.
+        tabuleiro[navio3_Linha_Inicial + i][navio3_Coluna_Inicial + i] = navio;
+    }
+
+    //Posicionamento Navio 4. (Diagonal 2)
+
+    //Verifica se o navio não sai dos limites do Tabuleiro.
+    if(navio4_Linha_Inicial - navio > LINHAS && navio4_Coluna_Inicial - navio > COLUNAS){
+        printf("Erro: o navio sai dos limites do Tabuleiro! \n");
+        return 1;
+    }
+
+    //Verifica se alguma posição está ocupada.
+    for(int i = 0; i < navio; i++){
+
+        if(tabuleiro[navio4_Linha_Inicial - i][navio4_Coluna_Inicial - i] == navio){
+            printf("Erro: Um navio já está ocupando esta posição. \n");
+            return 1;
+        }
+    }
+
+    // Se as verificações não mostrou erros, o navio será posicionado
+
+    for(int i = 0; i < navio; i ++){
+
+        // Posiciona o navio na vertical a partir da linha e coluna inicial.
+        tabuleiro[navio4_Linha_Inicial - i][navio4_Coluna_Inicial - i] = navio;
+    }
+
     // Exibindo o Tabuleiro
 
-    printf("=== TABULEIRO BATALHA NAVAL === \n");
+    printf("=== TABULEIRO BATALHA NAVAL ===");
 
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < LINHAS; i++){
 
-        for(int j = 0; j < 10; j++){
+        for(int j = 0; j < COLUNAS; j++){
 
             printf("%d ", tabuleiro[i][j]);
 
@@ -87,6 +150,5 @@ int main (){
 
     }
     
-
     return 0;
 }
